47e8badd50873e34caf9dc228d499c38f49500eb
WinoGrono11 <winogrona111@gmail.com>
2026-02-17 22:56:31 +0100

    Create Po Co page with mission and motivation

---
+++ pages/po-co.js
@@ -0,0 +1,6 @@
+// Po Co Page
+
+const mission = "Our mission is to empower individuals through knowledge and community engagement.";
+const motivation = "We believe in the strength of collaboration and the impact of informed choices.";
+
+export { mission, motivation };
