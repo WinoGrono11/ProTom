910747fb07fc6857c3241a914b1044f3df4d1ff7
WinoGrono11 <winogrona111@gmail.com>
2026-02-17 22:56:24 +0100

    Create Dla Kogo page about target customers

---
+++ pages/dla-kogo.js
@@ -0,0 +1,16 @@
+// Dla Kogo Page
+
+export default function DlaKogo() {
+  return (
+    <div>
+      <h1>Dla Kogo</h1>
+      <p>This page is dedicated to our target customers. Here, we describe their needs and how our services cater to them.</p>
+      <ul>
+        <li>Individuals interested in sustainable living</li>
+        <li>Wine enthusiasts looking for unique products</li>
+        <li>People seeking personalized wine experiences</li>
+      </ul>
+      <p>We strive to meet the needs of these customers through our dedicated services and products.</p>
+    </div>
+  );
+}
