3a97d2cf405e02a9b95f14ecffd5e065750bb2b8
WinoGrono11 <winogrona111@gmail.com>
2026-02-17 22:55:14 +0100

    Create next.config.js file for Next.js project

---
+++ next.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // additional custom configurations can be added here
+};
+
+module.exports = nextConfig;
